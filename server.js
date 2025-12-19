b4e55ce408c6a623972952a75fd073dd7c862378
OWU131323241 <s1323241@cst.otsuma.ac.jp>
2025-12-19 06:40:27 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("sensor", (data) => {
+    io.emit("sensor", data); // 全員に送信
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
